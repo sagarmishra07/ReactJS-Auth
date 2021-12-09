181d06247c3b33b6d5561ce0acc158b1f7800a7d
sagar mishra <sagarmishraswor@gmail.com>
2021-12-09 11:03:40 +0545

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhYZ-Ko4bYr9pEyDiquXRMGu8DGr-M_yk",
+  authDomain: "resourcesearch-dad50.firebaseapp.com",
+  projectId: "resourcesearch-dad50",
+  storageBucket: "resourcesearch-dad50.appspot.com",
+  messagingSenderId: "991920636489",
+  appId: "1:991920636489:web:7bb7103510bf3c8fb27598",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+
+export const logout = () => {
+  auth.signOut();
+};
+
+export { app, auth, db };
